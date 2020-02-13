122702a5c6be50c1a6fc5f409fa7c94905d86493
superpigs <1961181386@qq.com>
2020-02-13 19:29:44 +0800

    再次修改

---
+++ src/main/java/com/zhulei/test/entity/Clazz.java
@@ -0,0 +1,6 @@
+package com.zhulei.test.entity;
+
+public class Clazz {
+    private String clazz;
+    private String name;
+}
--- src/main/java/com/zhulei/test/entity/Student.java
+++ src/main/java/com/zhulei/test/entity/Student.java
@@ -1,5 +1,7 @@
 package com.zhulei.test.entity;
 
 public class Student {
-
+    private String id;
+    private String name;
+    private String age;
 }
